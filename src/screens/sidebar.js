8c9515fde9f2eed03785e9405e511ac58363d656
truong nguyen <n.h.t.94.e@gmail.com>
2020-03-31 00:26:43 +0700

    update sidebar

--- src/screens/sidebar.js
+++ src/screens/sidebar.js
@@ -17,7 +17,8 @@ export default function Sidebar() {
       <li key={"nav-" + i + "-" + nav[i].slug}>
         <a
           className={
-            "link noul flex c333" + (currentPage == nav[i].slug ? " on" : "")
+            "aic link noul flex c333" +
+            (currentPage === nav[i].slug ? " on" : "")
           }
           href={nav[i].slug}
         >
@@ -34,6 +35,38 @@ export default function Sidebar() {
       </a>
 
       <ul className="nav">{navigation}</ul>
+
+      <div className="updated-course flex aic">
+        <div className="icon-lamp-bright cfff s24 ico" />
+        <div className="lbl s15 fontb c333">
+          Updated Courses
+          <h2 className="author s13 c777">by NHT</h2>
+        </div>
+      </div>
+
+      <div className="stats aic flex">
+        <div className="stats-box flex">
+          <div className="ico ico-points s24 icon-shield" />
+          <h2 className="val s15 c333 fontb">17</h2>
+          <h2 className="lbl s13 c777">points</h2>
+        </div>
+
+        <div className="stats-box flex">
+          <div className="ico ico-battery s24 icon-battery-90" />
+          <h2 className="val s15 c333 fontb">45.3%</h2>
+          <h2 className="lbl s13 c777">points</h2>
+        </div>
+      </div>
+
+      <div className="me flex aic">
+        <div className="photo cfff s24">
+          <img src="http://placeimg.com/100/100/people" alt="me" />
+        </div>
+        <div className="lbl s15 fontb c333">
+          NHT
+          <h2 className="uname s13 c777">@delta94</h2>
+        </div>
+      </div>
     </div>
   );
 }
